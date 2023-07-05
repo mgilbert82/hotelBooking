5774de66f6e4a4065e15eaf494132d715681c4a6
Michel GILBERT <michel.1.gilbert@arval.fr>
2023-07-05 10:34:46 +0200

    Add routes files

---
+++ api/routes/auth.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { login, register } from '../controllers/authController.js';
+
+const router = express.Router();
+
+//Register routes
+router.post('/register', register);
+
+//Login routes
+router.post('/login', login);
+
+export default router;
