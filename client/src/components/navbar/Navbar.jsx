0258e6764ac2765f37a8a992001802b79a6e5024
Michel GILBERT <michel.1.gilbert@arval.fr>
2023-08-07 16:38:24 +0200

    Add navbar component

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faKey } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+  return (
+    <>
+      <div className='bg-blue-400 flex justify-center'>
+        <nav className='w-full flex justify-between align-items-center p-5 max-w-screen-xl text-white'>
+          {/* Logo */}
+          <a href='/'>
+            <span className='text-lg font-bold ms-4'>
+              <FontAwesomeIcon icon={faKey} className='me-2' />
+              HotelBooking
+            </span>
+          </a>
+          {/* Register & Login Button */}
+          <div className='flex justify-center align'>
+            <button className='bg-white rounded-md ml-2 py-1 px-2 text-blue-500 text-sm shadow-sm shadow-blue-700 cursor-pointer hover:bg-blue-500 hover:text-white'>
+              Register
+            </button>
+            <button className='bg-white rounded-md ml-2 py-1 px-2 text-blue-500 text-sm shadow-sm shadow-blue-700 cursor-pointer hover:bg-blue-500 hover:text-white'>
+              Login
+            </button>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
