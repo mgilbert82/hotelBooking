d9c356ea69eab24d376f62037d800d4ff73f8f52
Michel GILBERT <michel.1.gilbert@arval.fr>
2023-08-21 16:49:37 +0200

    Modify link to homepage

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -7,12 +8,12 @@ const Navbar = () => {
       <div className="bg-blue-400 flex justify-center">
         <nav className="w-full flex justify-between align-items-center p-5 max-w-screen-xl text-white">
           {/* Logo */}
-          <a href="/">
+          <Link to="/">
             <span className="text-lg font-bold ms-4">
               <FontAwesomeIcon icon={faKey} className="me-2" />
               HotelBooking
             </span>
-          </a>
+          </Link>
           {/* Register & Login Button */}
           <div className="flex justify-center align">
             <button className="bg-white rounded-md ml-2 py-1 px-2 text-blue-500 text-sm shadow-sm shadow-blue-700 cursor-pointer hover:bg-blue-500 hover:text-white">
